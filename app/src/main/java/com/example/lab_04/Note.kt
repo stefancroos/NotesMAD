0f77cea1665bf8d834fc309430aefdb8cbf29bbd
Vinthiya <vinthiyaperi14@gmail.com>
2024-05-12 18:14:06 +0530

    finished project

---
+++ app/src/main/java/com/example/lab_04/Note.kt
@@ -0,0 +1,6 @@
+package com.example.lab_04
+
+import android.icu.text.CaseMap.Title
+import androidx.activity.result.contract.ActivityResultContracts
+
+data class Note(val id: Int, val title: String, val content: String)
